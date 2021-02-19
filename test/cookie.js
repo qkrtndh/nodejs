fad094e7dba454f2d9bfad2877c949b187ed49fe
qkrtndh <qkrtndh5@naver.com>
2021-02-19 19:27:14 +0900

    쿠키 읽기

--- test/cookie.js
+++ test/cookie.js
@@ -1,5 +1,13 @@
 var http = require('http');
+var cookie = require('cookie');
+
 http.createServer((request, response) => {
-    //response.writeHead(200,{'Set-cookie':['yummy_cookie=choco','tasty_cookie=strawbarry']});
+    var cookies = {};//조건문 안에서 만들면 밖에서 못쓰므로 미리 만듬
+    if(request.headers.cookie!=undefined)
+    {
+        cookies=cookie.parse(request.headers.cookie);
+    }
+    console.log(cookies.yummy_cookie);
+    response.writeHead(200,{'Set-cookie':['yummy_cookie=choco','tasty_cookie=strawbarry']});
     response.end('cookie');
 }).listen(3000);
