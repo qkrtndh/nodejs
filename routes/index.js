65e5831ff6eba3bfde7463c1a129aa636a76eaa5
qkrtndh <qkrtndh5@naver.com>
2021-02-22 19:05:46 +0900

    로그인완성

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,33 @@
 var express = require('express');
 var router = express.Router();
 var template = require('../lib/template.js');
-//app.get('/',(req,res)=>res.send('Hello world'))
-router.get('/', function (request, response) {//get방식으로 입력된 주소를 라우팅
 
-    var title = "welcome";
-    var description = "main page";
-    var list = template.List(request.list);
-    var HTML = template.HTML(title, list,
-      `<h2>${title}</h2>${description} 
+function authIsOwner(request, response) {
+  if (request.session.is_login) {
+    return true;
+  }
+  return false;
+}
+
+function authStatusUI(request,response)
+{
+  var authStatusUI = '<a href="/auth/login">login</a>';
+  if (authIsOwner(request, response)) {
+    authStatusUI = `${request.session.nickname}|<a href="/auth/logout">logout</a>`;
+  }
+  return authStatusUI;
+}
+router.get('/', function (request, response) {//get방식으로 입력된 주소를 라우팅
+  var title = "welcome";
+  var description = "main page";
+  var list = template.List(request.list);
+  var HTML = template.HTML(title, list,
+    `<h2>${title}</h2>${description} 
       <img src=/images/hello.jpg style="width : 300px; display:block;margin-top:10px;">
       `,
-      `<a href="/topic/create">creat</a>`);
-    response.send(HTML);
-  
-  })
+    `<a href="/topic/create">creat</a>`,authStatusUI(request,response));
+  response.send(HTML);
 
-  module.exports = router;
+})
+
+module.exports = router;
